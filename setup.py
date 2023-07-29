236c288878686375b0e292ee8ddc4e002ba2ca5b
MaximilianPowers <maximilianjpowers@gmail.com>
2023-07-29 12:53:17 +0100

    Added manifold capacity metric code

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 import os
-base = 'models'
+base = 'metrics'
 sub_folders = os.listdir(base)
+
 for folder in sub_folders:
-    if len(folder.split('.')) > 1:
-        continue
-    models = os.listdir(os.path.join(base, folder))
-    for model in models:
-        if len(model.split('.')) > 1:
-            continue
-        os.mkdir(os.path.join(base, folder, model, 'logs'))
-        open(os.path.join(base, folder, model, 'train.py'), 'x').close()
-        open(os.path.join(base, folder, model, 'test.py'), 'x').close()
+    if os.path.isdir(os.path.join(base, folder)):
+        os.mkdir(os.path.join(base, folder, 'utils'))
